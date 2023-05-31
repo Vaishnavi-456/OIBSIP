cfb6bf1776188f6448a90b88a1a94581e0d6cda3
Vaishnavi-456 <133573956+Vaishnavi-456@users.noreply.github.com>
2023-05-31 11:13:16 +0530

    Delete Portfolio Website directory

--- Portfolio Website/portfolio.js
+++
@@ -1,9 +0,0 @@
-// Add a fade-in effect to the sections
-const sections = document.querySelectorAll('section');
-sections.forEach((section, index) => {
-  section.style.opacity = 0;
-  setTimeout(() => {
-    section.classList.add('fade-in');
-    section.style.opacity = 1;
-  }, index * 300);
-});
